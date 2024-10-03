9c6a91820f9b55c687d0a04c9a0288f71bb4aae4
loop0919 <ruupu2001@gmail.com>
2024-10-03 23:04:41 +0900

    定数化

--- routers/route_problem.py
+++ routers/route_problem.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Request, Response
 from fastapi.encoders import jsonable_encoder
-from starlette.status import HTTP_201_CREATED, HTTP_500_INTERNAL_SERVER_ERROR
+from starlette.status import (HTTP_201_CREATED, HTTP_404_NOT_FOUND,
+                              HTTP_500_INTERNAL_SERVER_ERROR)
 
 from database import (db_create_problem, db_delete_problem, db_get_problem,
                       db_get_problems, db_update_problem)
@@ -29,7 +30,7 @@ async def get_problem(id: str):
     problem = await db_get_problem(id)
     if problem:
         return problem
-    raise HTTPException(status_code=404, detail=f"Problem of ID:{id} not found")
+    raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"Problem of ID:{id} not found")
 
 
 @router_problem.put("/api/problem/{id}", response_model=Problem)
@@ -38,7 +39,7 @@ async def update_problem(id: str, problem: ProblemBody):
     res = await db_update_problem(id, problem)
     if res:
         return res
-    raise HTTPException(status_code=404, detail=f"Failed to Update Problem of ID:{id}")
+    raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"Failed to Update Problem of ID:{id}")
 
 
 @router_problem.delete("/api/problem/{id}", response_model=SuccessMsg)
@@ -46,4 +47,4 @@ async def delete_problem(id: str):
     res = await db_delete_problem(id)
     if res:
         return {"message": f"Problem of ID:{id} deleted"}
-    raise HTTPException(status_code=404, detail=f"Failed to Delete Problem of ID:{id}")
+    raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"Failed to Delete Problem of ID:{id}")
